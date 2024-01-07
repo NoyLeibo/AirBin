99e395a6d9c6bc198ba33c91631d048aa30bee92
Iris Yatskaer <iriska1160@gmail.com>
2024-01-07 11:57:39 +0200

    Last push before meeting

--- sprint4-frontend/src/pages/StayDetails.jsx
+++ sprint4-frontend/src/pages/StayDetails.jsx
@@ -57,14 +57,32 @@ export function StayDetails() {
             {stay.capacity} guests, 2 rooms, 2 beds, 1 bath
           </h4>
           <span className="sticky-card"></span>
-          <div className="stay-dets-highlights">
+          <div className="stay-dets-highlights-first">
             <h3>Great location</h3>
             <div>100% of recent guests gave the location a 5-star rating.</div>
             <h3>Great check-in experiance</h3>
             <div>
               100% of recent guests gave the check-in process a 5-star rating.
             </div>
-            <h3>Free cancellation before Jan 8</h3>
+            <h3 className="divider">Free cancellation before Jan 8</h3>
+            <img
+              className="aircover"
+              src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg"
+              alt="aircover"
+            ></img>
+            <div className="divider">
+              Every booking includes free protection from Host cancellations,
+              listing inaccuracies, and other issues like trouble checking in.
+            </div>
+            <div className="stay-owner-revirew">
+              Moshe\'s house, stays under 7 night $38/res - Inquire about
+              availability, I review then offer/approve if available :) - READ
+              "The Space" for cleaning/etc AND brief explanation about timeshare
+              reservations - Want guaranteed view for additional cost? Must be
+              weekly rental, other restrictions - Wheelchair accessible / ADA,
+              call resort directly to ensure U receive. If U need ADA U MUST
+              inform us BEFORE booking.
+            </div>
           </div>
           <StickyCard stay={stay} />
         </div>
