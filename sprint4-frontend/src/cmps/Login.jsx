31c7dc3891eed1f322e3fb5cbb5a68b9ce67511f
Iris Yatskaer <iriska1160@gmail.com>
2024-01-09 13:08:44 +0200

    Add user to local session

--- sprint4-frontend/src/cmps/Login.jsx
+++ sprint4-frontend/src/cmps/Login.jsx
@@ -1,109 +1,126 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { userService } from '../services/user.service';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userService } from "../services/user.service";
 
 export function LoginModal() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [fullname, setFullname] = useState('');
-    const [isRegistering, setIsRegistering] = useState(false);
-    const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
+  const navigate = useNavigate();
 
-    const demoUser = { username: 'NoyLeibo', password: '123' };
-    const demoLogin = () => {
-        setUsername(demoUser.username);
-        setPassword(demoUser.password);
-    };
+  const demoUser = { username: "NoyLeibo", password: "123" };
+  const demoLogin = () => {
+    setUsername(demoUser.username);
+    setPassword(demoUser.password);
+  };
 
-    const handleLoginSubmit = async (event) => {
-        event.preventDefault();
-        // Login logic here
-        console.log(`try to log in user: Username: ${username}, Password: ${password}`);
-        try {
-            const user = await userService.login({ username, password })
-            console.log(`Logging in user: 
-                        Username: ${username}, 
-                        Password: ${password}`);
-            navigate('/');
-        } catch (err) {
-            console.log('Login failed:', err);
-        }
-    };
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await userService.logout();
+      await userService.login({ username, password });
+      navigate("/");
+    } catch (err) {
+      console.log("err: " + err);
+    } finally {
+      if (!userService.getLoggedinUser()) {
+        alert("Wrong username or password");
+      }
+    }
+  };
 
-    const handleRegisterSubmit = async (event) => {
-        event.preventDefault();
-        // Registration logic here
-        console.log(`Registering user: Username: ${username}, Password: ${password}`);
-        try {
-            const user = await userService.signup({ fullname: username, username, password, balance: 10000, isAdmin: false })
-            navigate('/');
-        } catch (err) {
-            console.log('Registration failed:', err);
-        }
-    };
+  const handleRegisterSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await userService.signup({
+        fullname: username,
+        username,
+        password,
+        balance: 10000,
+        isAdmin: false,
+      });
+      navigate("/");
+    } catch (err) {
+      console.log("Registration failed:", err);
+    }
+  };
 
-    const toggleForm = () => {
-        setIsRegistering(!isRegistering);
-    };
+  const toggleForm = () => {
+    setIsRegistering(!isRegistering);
+  };
 
-
-    return (
-        <div className="login-modal">
-            <div className='login-modal-title flex align-center justify-center bold'>
-                {isRegistering ? 'Signup' : 'Login'}
-            </div>
-            <div>Welcome to Airbmb</div>
-            <form className='flex column' onSubmit={isRegistering ? handleRegisterSubmit : handleLoginSubmit}>
-                {isRegistering && (<div className="input-group">
-                    <label htmlFor="Fullname"><span className='red'>*</span> Fullname</label>
-                    <input
-                        type="text"
-                        id="Fullname"
-                        name="Fullname"
-                        required
-                        minLength="3"
-                        maxLength="20"
-                        value={fullname}
-                        onChange={(e) => setFullname(e.target.value)}
-                    />
-                </div>)}
-                <div className="input-group">
-                    <label htmlFor="username"><span className='red'>*</span> Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        required
-                        minLength="3"
-                        maxLength="20"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password"><span className='red'>*</span> Password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        required
-                        minLength="6"
-                        maxLength="20"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button className="secondary-button" type="button" onClick={toggleForm}>
-                    {isRegistering ? 'Already a member? Login' : 'Not a member? Register'}
-                </button>
-                <button type="submit">{isRegistering ? 'Register' : 'Log In'}</button>
-                {!isRegistering && (
-                    <button type="button" className="demo-login-button" onClick={demoLogin}>
-                        Demo Login
-                    </button>
-                )}
-            </form>
+  return (
+    <div className="login-modal">
+      <div className="login-modal-title flex align-center justify-center bold">
+        {isRegistering ? "Signup" : "Login"}
+      </div>
+      <div>Welcome to Airbmb</div>
+      <form
+        className="flex column"
+        onSubmit={isRegistering ? handleRegisterSubmit : handleLoginSubmit}
+      >
+        {isRegistering && (
+          <div className="input-group">
+            <label htmlFor="Fullname">
+              <span className="red">*</span> Fullname
+            </label>
+            <input
+              type="text"
+              id="Fullname"
+              name="Fullname"
+              required
+              minLength="3"
+              maxLength="20"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="input-group">
+          <label htmlFor="username">
+            <span className="red">*</span> Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            required
+            minLength="3"
+            maxLength="20"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
-    );
+        <div className="input-group">
+          <label htmlFor="password">
+            <span className="red">*</span> Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            minLength="6"
+            maxLength="20"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="secondary-button" type="button" onClick={toggleForm}>
+          {isRegistering ? "Already a member? Login" : "Not a member? Register"}
+        </button>
+        <button type="submit">{isRegistering ? "Register" : "Log In"}</button>
+        {!isRegistering && (
+          <button
+            type="button"
+            className="demo-login-button"
+            onClick={demoLogin}
+          >
+            Demo Login
+          </button>
+        )}
+      </form>
+    </div>
+  );
 }
-
