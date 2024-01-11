0010c12c6040d378806b0158d464fa1d9a4bb2d0
Iris Yatskaer <iriska1160@gmail.com>
2024-01-11 19:33:11 +0200

    Postman is working

--- sprint4-backend/server.js
+++ sprint4-backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { authRoutes } from "./api/auth/auth.routes.js";
+import { userRoutes } from "./api/user/user.routes.js";
+
 const app = express();
 const port = 3031;
 
 const corsOptions = {
   origin: [
-    "http://127.0.0.1:8080",
-    "http://localhost:8080",
-    "http://127.0.0.1:5175",
-    "http://localhost:5175",
-    "http://127.0.0.1:5173",
-    "http://localhost:5173",
-    "http://127.0.0.1:5174",
-    "http://localhost:5174",
+    // "http://127.0.0.1:8080",
+    // "http://localhost:8080",
+    // "http://127.0.0.1:5175",
+    // "http://localhost:5175",
+    // "http://127.0.0.1:5173",
+    // "http://localhost:5173",
+    // "http://127.0.0.1:5174",
+    // "http://localhost:5174",
   ],
   credentials: true,
 };
@@ -21,6 +24,9 @@ app.use(cors(corsOptions));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
